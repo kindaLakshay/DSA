e8b2faea40c3e623d01792d0e538fd4b17b018c5
Lakshay Choudhary <lakshaychoudhary77712@gmail.com>
2023-09-01 19:13:51 +0530

    added some files

---
+++ SelectionSort.cpp
@@ -0,0 +1,23 @@
+// https://www.codingninjas.com/studio/problems/selection-sort_981162?source=youtube&campaign=love_babbar_codestudio2&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio2
+// Selection Sort
+// total rounds = (n-1)
+// time complexity
+
+// conside arr[0] to be small and then iterate and compate and swap each larger one
+
+#include<vector>
+using namespace std;
+void selectionSort(vector<int>& arr, int n)
+{   
+    for(int i = 0; i < n-1; i++ ) {
+        int minIndex = i;
+        
+        for(int j = i+1; j<n; j++) {
+            
+            if(arr[j] < arr[minIndex]) 
+                minIndex = j;
+            
+        }
+        swap(arr[minIndex], arr[i]);
+    }
+}
