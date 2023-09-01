f1383c7ab1b29d292f825241dbdeebd32f5401f5
Lakshay Choudhary <lakshaychoudhary77712@gmail.com>
2023-09-01 19:20:49 +0530

    added TC and SC data to SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,7 +1,8 @@
 // https://www.codingninjas.com/studio/problems/selection-sort_981162?source=youtube&campaign=love_babbar_codestudio2&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio2
 // Selection Sort
 // total rounds = (n-1)
-// time complexity
+// time complexity = O(n**2)
+// space complexity = O(1) 
 
 // conside arr[0] to be small and then iterate and compate and swap each larger one
 
