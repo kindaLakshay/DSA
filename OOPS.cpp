d758eaacf40986bf85c341c303d712bd377d449d
Lakshay Choudhary <lakshaychoudhary77712@gmail.com>
2023-09-27 14:32:15 +0530

    added files

---
+++ OOPS.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+	
+class Hero{
+	// empty class gets 1 byte, just to 
+	// keep trach of it
+
+	char name[100];
+	int health;
+	int damage;
+};
+
+int main(){
+    Hero h1;
+    // cout<<"size: "<<sizeof(h1)<<endl;
+    cout<<"health is: "<<h1.health
+}
